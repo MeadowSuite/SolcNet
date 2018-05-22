71627010d931848a1580f55c12c5bfd7d6d885f6
Matt Little <zone117x@gmail.com>
2018-05-22 10:06:16 -0700

    Stub MSBuild task in SolCodeGen - pending nuget installation testing; msbuild tasks my be too limited and may switch to implementing an dotnet build tool

---
+++ SolCodeGen/ContractGeneratorTask.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolCodeGen
+{
+    public class ContractGeneratorTask : Task
+    {
+        public string MyProperty { get; set; }
+
+        public override bool Execute()
+        {
+            Log.LogWarning("Example mesage\n\n\n\n\n\nNEW LINE");
+            return true;
+        }
+
+        
+    }
+}
